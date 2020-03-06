b9c2e94b8380cbffb17c9c4ba461ce1458a95aff
amitisZeini <aberry14.002@gmail.com>
2020-03-05 22:06:02 -0800

    added manipulation segment

--- 2020_InfiniteRecharge_Data/CSVDataAnalysis.py
+++ 2020_InfiniteRecharge_Data/CSVDataAnalysis.py
@@ -6,11 +6,12 @@ file_name = "2020calndata"
 output_title = ["Team", "Auto Init Points", "Auto Cells", "Tele Cells", "Control Panel", "End Game Points", "Score"]
 
 # List of weights for each corresponding column
-auto_init_wt = 0
-auto_cell_wt = 0
-tele_cell_wt = 0
-control_panel_wt = 0
-end_game_wt = 0
+auto_init_wt = 5
+auto_cell_wt = 4
+tele_cell_wt = 2
+control_panel_wt = 15
+end_game_wt = 25
+score = 0
 
 weights = [auto_init_wt, auto_cell_wt, tele_cell_wt, control_panel_wt, end_game_wt]
 
@@ -36,6 +37,14 @@ with open(file_name + ".csv") as csv_file:
             team_data.append([row[0][3:]] + row[1:])
 
 # Manipulate data here, team data is list of team data
+
+for i in range(0,len(team_data)):
+    score = 0
+    for j in range(0,len(team_data[i])):
+        if j != 0:
+            team_data[j] = team_data[j] * weights[j-1]
+            score += team_data[j]
+    team_data.append(score)
 
 
 
